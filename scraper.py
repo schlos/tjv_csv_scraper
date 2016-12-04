50ffb6449a1f987c37d6ce72018752a5c2724822
Miroslav <schlos@users.noreply.github.com>
2016-12-04 21:50:23 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,7 +8,7 @@ data = data.splitlines()
 reader = csv.DictReader(data)
 
 for row in reader:
-    scraperwiki.sql.save(data=row)
+    scraperwiki.sqlite.save(unique_keys=["OIB"], data=row)
 
     #record['Name'] = record['Name'].decode("cp1252")
     #print "%s, %s last updated on %s" % (row[2], row[3], row[16])
