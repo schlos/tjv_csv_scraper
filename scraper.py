b192db8756e69e2b0eb0e4e7ea60859fb5896f0c
Miroslav <schlos@users.noreply.github.com>
2019-07-17 22:46:32 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@ import scraperwiki
 #url = 'http://schlos.net/codeforcroatia.org/tijela-2019-07-17.csv'
 
 # Comma separated CSV:
-url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDRAq7dLGma-4y09syGLI7l8tacTWCu70HE7YBFLUI74dgth99kg_MPEz4ZJWi5wwuCi48bXBgiSMc/pub?gid=1362161529&single=true&output=csv'
+url = 'http://schlos.net/codeforcroatia.org/tijela-2017-test-comma.csv'
 
 data = scraperwiki.scrape(url)
 data = data.splitlines()
@@ -15,8 +15,8 @@ reader = csv.DictReader(data)
 for row in reader:
     print row
     #scraperwiki.sqlite.save(unique_keys, data=row)
-    scraperwiki.sqlite.save(['Rb.'], row)
-    #scraperwiki.sqlite.save(row[1], data=row)
+    scraperwiki.sqlite.save(unique_keys=['Rb.'], data=row)
+    #scraperwiki.sqlite.save(unique_keys=['OIB', 'Rb.'], data=record)
 
     #record['Name'] = record['Name'].decode("cp1252")
     #print "%s, %s last updated on %s" % (row[2], row[3], row[16])
