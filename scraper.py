9fd702ba4ddb7d939dc7cd5a5fddb3e986eb69e8
Miroslav <schlos@users.noreply.github.com>
2019-07-17 22:17:36 +0200

    fixes using scraping.pro guide
    http://scraping.pro/how-to-scrape-csv-data-files/

--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
-import scraperwiki
 import csv
+import scraperwiki
 
-url = 'http://tjv.pristupinfo.hr/?sort=1&page=1&download'
+#url = 'http://tjv.pristupinfo.hr/?sort=1&page=1&download'
+url = 'http://schlos.net/codeforcroatia.org/tijela-2019-07-17.csv'
 
 data = scraperwiki.scrape(url)
 data = data.splitlines()
 reader = csv.DictReader(data)
 
 for row in reader:
-    scraperwiki.sqlite.save(unique_keys, data=row)
+    print row
+    #scraperwiki.sqlite.save(unique_keys, data=row)
+    scraperwiki.sqlite.save(['OIB'], row)
 
     #record['Name'] = record['Name'].decode("cp1252")
     #print "%s, %s last updated on %s" % (row[2], row[3], row[16])
