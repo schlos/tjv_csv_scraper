1183c6bbaa2a0eb7257fe49a9b65be878037ca59
Miroslav <schlos@users.noreply.github.com>
2019-07-17 22:38:06 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 import csv
 import scraperwiki
 
-url = 'http://tjv.pristupinfo.hr/?sort=1&page=1&download'
+# Semicolon separated CSV:
+#url = 'http://tjv.pristupinfo.hr/?sort=1&page=1&download'
 #url = 'http://schlos.net/codeforcroatia.org/tijela-2019-07-17.csv'
+
+# Comma separated CSV:
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQDRAq7dLGma-4y09syGLI7l8tacTWCu70HE7YBFLUI74dgth99kg_MPEz4ZJWi5wwuCi48bXBgiSMc/pub?gid=1362161529&single=true&output=csv'
 
 data = scraperwiki.scrape(url)
 data = data.splitlines()
